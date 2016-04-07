e7d4f281d844d4d90aa637d0c22feb73670576e8
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-07 16:01:16 -0500

    Updates User to add canvas_id + data per spec

--- serious_games_framework/users/models.py
+++ serious_games_framework/users/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import JSONField
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -14,6 +15,8 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    canvas_id = models.IntegerField(blank=True, null=True)
+    data = JSONField(blank=True, null=True)
 
     def __str__(self):
         return self.username
