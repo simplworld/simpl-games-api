8aaff8709dd0e0f857fac58eefdb2b3f466d1161
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-22 12:01:58 -0500

    Lints serializers

--- simpl/games/apis/serializers.py
+++ simpl/games/apis/serializers.py
@@ -1,5 +1,4 @@
-from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer, Serializer
+from rest_framework.serializers import ModelSerializer
 
 from .. import models
 
