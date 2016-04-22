d3218086644fccd4746a3a4cb4b1b140047022e4
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-22 10:35:29 -0500

    Updates game views to redirect to game_list

--- serious_games_framework/simpl/views/game.py
+++ serious_games_framework/simpl/views/game.py
@@ -1,8 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 from django.core.urlresolvers import reverse
-from django.core.urlresolvers import reverse_lazy
-from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
@@ -18,7 +16,7 @@ class GameCreateView(SuccessMessageMixin, CreateView):
     success_message = '%(name)s was created successfully'
 
     def get_success_url(self):
-        return reverse('simpl:game_detail', args=(self.object.pk,))
+        return reverse('simpl:game_list')
 
 
 class GameDeleteView(DeleteView):
@@ -46,7 +44,7 @@ class GameListView(ListView):
     allow_empty = True
 
 
-class GameUpdateView(UpdateView):
+class GameUpdateView(SuccessMessageMixin, UpdateView):
     model = Game
     form_class = GameForm
     template_name = 'simpl/games/game_update.html'
@@ -54,4 +52,4 @@ class GameUpdateView(UpdateView):
     success_message = '%(name)s was updated successfully'
 
     def get_success_url(self):
-        return reverse('simpl:game_detail', args=(self.object.pk,))
+        return reverse('simpl:game_list')
