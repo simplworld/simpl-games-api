258d11518a7886cfeaed981ab85d43971d861ca9
Stephen D. Spencer <stephen@revsys.com>
2016-10-10 14:33:00 -0500

    templates dict adjustments

--- config/settings/prod_salt.py
+++ config/settings/prod_salt.py
@@ -65,6 +65,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
     ('django.template.loaders.cached.Loader', [
         'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
 ]
+TEMPLATES[0]['DIRS']['/home/simpl_api/app/templates']
 
 # DATABASE CONFIGURATION
 # ------------------------------------------------------------------------------
