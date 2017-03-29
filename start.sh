ac59fa69a070b487915dcdbcfcdc047692630b6c
Frank Wiles <frank@revsys.com>
2017-03-29 06:01:32 -0500

    Change logging

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn --log-config=/code/config/logging.conf -b 0.0.0.0:80 -w 2 --name simpl 'config.wsgi'
+gunicorn -b 0.0.0.0:80 -w 2 --name simpl 'config.wsgi'
