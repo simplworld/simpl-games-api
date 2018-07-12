3591b7445a044a0628a5108bea1831e38baff385
Flavio Curella <flavio.curella@gmail.com>
2018-07-12 10:33:25 -0500

    Fix AppConfig

--- config/settings/common.py
+++ config/settings/common.py
@@ -53,7 +53,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'simpl_users',  # custom users app
-    'simpl.games',
+    'simpl.games.apps.SimplGamesConfig',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- simpl/games/apps.py
+++ simpl/games/apps.py
@@ -7,12 +7,12 @@ from . import events
 
 
 class SimplGamesConfig(AppConfig):
-    name = 'games'
+    name = 'simpl.games'
 
     def ready(self):
         webhook_model(get_user_model(),
             on_change=events.on_user_changed,
             on_create=events.on_user_created,
             on_delete=events.on_user_deleted,
-            reverse=model_reverser('simpl_api:user-detail', pk='pk'),
+            reverse=model_reverser('simpl_users_api:user-detail', id='pk'),
         )
