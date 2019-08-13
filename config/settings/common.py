82e8efbdf3b0e53442471b3d14f02dfb3f0292eb
Jeff Triplett <jeff.triplett@gmail.com>
2019-08-13 15:36:42 -0500

    :fire: Removes unused deps

--- config/settings/common.py
+++ config/settings/common.py
@@ -9,7 +9,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 from __future__ import absolute_import, unicode_literals
-import os
+
 import environ
 
 ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
