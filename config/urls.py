100b13f9d7109b3eed73aaa4bf46d36f880c4d71
Frank Wiles <frank@revsys.com>
2020-08-09 14:50:04 -0500

    Upgrade to latest simpl-users

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from simpl.games.apis.bulk_urls import bulk_router as bulk_api_router
 schema_view = get_schema_view(
     openapi.Info(
         title="Simpl Games API",
-        default_version='v1',
+        default_version="v1",
         description="Simpl Framework Games API",
         license=openapi.License(name="BSD License"),
     ),
@@ -27,60 +27,70 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-    url(settings.ADMIN_URL, admin.site.urls),
-]
+urlpatterns = [url(settings.ADMIN_URL, admin.site.urls)]
 
 # User management
 urlpatterns += [
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r"^accounts/", include("allauth.urls")),
+    url(r"^rest-auth/", include("rest_auth.urls")),
 ]
 
 # Static Media and User Uploads
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True)
+urlpatterns += static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=True
+)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Our application urls
 urlpatterns += [
-    url(r'^users/',
+    url(r"^users/", include(("simpl_users.apis.urls", "users"), namespace="users")),
+    url(r"^apis/", include((api_router.urls, "simpl_api"), namespace="simpl_api")),
+    url(
+        r"^apis/",
         include(
-            ('simpl_users.urls', 'users'),
-            namespace='users'
-        )),
-    url(r'^apis/',
-        include(
-            (api_router.urls, 'simpl_api'),
-            namespace='simpl_api'
-        )),
-    url(r'^apis/',
-        include(
-            (simpl_users_api_router.urls, 'simpl_users_api'),
-            namespace='simpl_users_api'
-        )),
-    url(r'^apis/hooks/',
-        include(
-            ("simpl.webhook.urls", "simpl_webhook"), namespace="webhook"
-        )),
-    url(r'^apis/bulk/',
-        include(
-            (bulk_api_router.urls, 'simpl_bulk_api'),
-            namespace='simpl_bulk_api'
-        )),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    url(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+            (simpl_users_api_router.urls, "simpl_users_api"),
+            namespace="simpl_users_api",
+        ),
+    ),
+    url(
+        r"^apis/hooks/",
+        include(("simpl.webhook.urls", "simpl_webhook"), namespace="webhook"),
+    ),
+    url(
+        r"^apis/bulk/",
+        include((bulk_api_router.urls, "simpl_bulk_api"), namespace="simpl_bulk_api"),
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+    url(
+        r"^(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"
+    ),
 ]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(r'^400/$', default_views.bad_request,
-            kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied,
-            kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found,
-            kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
+        url(
+            r"^400/$",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        url(
+            r"^403/$",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        url(
+            r"^404/$",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        url(r"^500/$", default_views.server_error),
     ]
