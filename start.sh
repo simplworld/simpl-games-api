bf9858056fa1e00a7b6e4fb591a3728582000086
Frank Wiles <frank@revsys.com>
2020-08-10 07:06:44 -0500

    Remove debugging detritus

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ./wait-for-it.sh -h db -p 5432 -t 20 -- python manage.py migrate
 python manage.py collectstatic --noinput
-#gunicorn -c /code/gunicorn.conf.py 'config.wsgi'
-python manage.py runserver 0.0.0.0:8000
+gunicorn -c /code/gunicorn.conf.py --reload 'config.wsgi'
