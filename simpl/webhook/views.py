09a820a563883bdd493ba3443af562aaad188660
Frank Wiles <frank@revsys.com>
2020-08-16 14:12:08 -0500

    Prevent models from registering multiple copies of their webhooks

--- simpl/webhook/views.py
+++ simpl/webhook/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import get_object_or_404
 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -5,6 +7,8 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Subscriber
 from .serializers import SubscriberSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class SubscriberList(ListCreateAPIView):
@@ -16,6 +20,15 @@ class SubscriberList(ListCreateAPIView):
         return self.model.objects.filter(user=self.request.user).order_by('id')
 
     def perform_create(self, serializer):
+        # Only create new instances, otherwise it's just a model service
+        # re-registering itself.
+        if self.model.objects.filter(
+            user=self.request.user,
+            event=self.request.data['event'],
+            url=self.request.data['url'],
+        ).exists():
+            return
+
         serializer.save(user=self.request.user)
 
 
