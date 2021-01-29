ca74c2d708555fecc1a078f6b4a288b4b1b40758
Frank Wiles <frank@revsys.com>
2021-01-29 15:21:48 -0600

    Fix db pooling JSONB issue
        - django-db-geventpool version was too high for this version of Django
        - Changed local development configuration to match deployments using pool to avoid future confusion/bugs
        - Removed unused environment variable in docker-compose

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,6 +12,19 @@ from .common import *  # noqa
 
 import logging
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django_db_geventpool.backends.postgresql_psycopg2",
+        "HOST": "db",
+        "USER": "postgres",
+        "NAME": "postgres",
+        "PASSWORD": "",
+        "PORT": 5432,
+        "ATOMIC_REQUESTS": False,
+        "CONN_MAX_AGE": 0,
+        "OPTIONS": {"MAX_CONNS": 50},
+    }
+}
 
 # DEBUG
 # ------------------------------------------------------------------------------
