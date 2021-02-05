acc484067798a4fd108b4d7ee15100d5b48019bb
Frank Wiles <frank@revsys.com>
2021-02-05 16:34:01 -0600

    Fix tests from local config change
    I made it easier to not make a mistake in local dev by allowing
    localhost and non-https webhooks work, but this broke a test.
    
    Specifically setting them off in the test settings.

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,19 +1,18 @@
 from .local import *  # noqa
 
+SIMPL_WEBHOOK_ALLOW_LOCALHOST = False
+SIMPL_WEBHOOK_ALLOW_HTTP = False
 # Speeding up the tests
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
     # 'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 
 
 class DisableMigrations(object):
-
     def __contains__(self, item):
         return True
 
